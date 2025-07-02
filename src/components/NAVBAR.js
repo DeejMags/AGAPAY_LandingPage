ef58a75395f465a24cfba743b26967ae6d049c98
DeejMags <rnmaglatangjr@gmail.com>
2025-07-02 12:05:34 +0800

    updated version but not yet final

--- src/components/NAVBAR.js
+++ src/components/NAVBAR.js
@@ -1,11 +1,12 @@
 import React from "react";
+import logo from "./imgs/agapay-logo.png";
 
 export default function NAVBAR() {
   return (
     <nav className="fixed top-6 left-1/2 transform -translate-x-1/2 w-[98vw] max-w-7xl z-50 px-8 py-4 flex justify-between items-center bg-white/70 backdrop-blur-md rounded-3xl shadow-lg border-2 border-teal-200">
-      {/* Logo (test with favicon) */}
+      {/* Logo (imported from src/components/imgs) */}
       <div className="flex items-center">
-        <img src="../assets/agapay-logo.png" alt="Test" className="w-10 h-10 object-contain mr-3" />
+        <img src={logo} alt="logo" className="w-10 h-10 object-contain mr-3" />
       </div>
       <ul className="hidden md:flex space-x-10 font-semibold text-black font">
         <li><a href="#hero" className="hover:text-teal-300 transition">Home</a></li>
